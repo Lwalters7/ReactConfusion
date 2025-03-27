0986f687a1ac9051361444731d27b0582dc1c71a
Luke Walters <lwalters@ualr.edu>
2025-03-27 14:20:09 -0500

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Menu from './components/MenuComponent'; // Import the Menu component
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Menu /> {/* Render the Menu component */}
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+    console.log("Dishes Loaded: ", this.state.dishes); // Debugging log
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to My App</h1> {/* Add this to check if the app renders */}
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
